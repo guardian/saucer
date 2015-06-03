2bd740acaa64e75ece530f489e514b10db74386a
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2015-06-03 13:49:22 +0100

    and templates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,11 +4,12 @@ var main = (function() {
     function getData(callback) {
         var url = 'http://visuals.guim.co.uk/spreadsheetdata/1-xaQYSi2vCehcIhs_p1bNWLed1PQb3nUSmVkRrOciFw.json';
         $.get(url, function(data) {
-            console.log(data);
+            callback(data.sheets.missions);
         });
     }
 
     function loadTemplates(data) {
+        console.log(data)
       $.get('templates/mission-teaser.mst', function(template) {
         var rendered = Mustache.render(template, data);
         $('#mission-teaser').html(rendered);
