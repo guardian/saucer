28498ca22568ab86fda682d151982cff65abfa91
Will Franklin <will.franklin@guardian.co.uk>
2015-06-03 16:10:51 +0100

    Send actual time

--- app/scripts/tag.js
+++ app/scripts/tag.js
@@ -17,7 +17,7 @@
 
     var item = [
       '<li>',
-        '<time>' + pad(mins) + ':' + pad(secs) + '</time>',
+        '<time data-time="' + time + '">' + pad(mins) + ':' + pad(secs) + '</time>',
         '<span>' + tag + '</span>',
         '<i class="glyphicon glyphicon-remove"></i>',
       '</li>'].join('');
@@ -48,9 +48,9 @@
 
   $('#save').click(function () {
     var data = $tags.children().map(function () {
-      var time = this.querySelector('time').textContent;
+      var time = this.querySelector('time').getAttribute('data-time');
       var tag = this.querySelector('span').textContent;
-      return '{"time": "' + time + '", "tag": "' + tag + '"}';
+      return '{"time": ' + time + ', "tag": "' + tag + '"}';
     }).get().join(',\n');
 
     $('#saved').text('[' + data  + ']');
