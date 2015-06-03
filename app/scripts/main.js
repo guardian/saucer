1bfbff4ddcc9d888649dbd1e333777a79505eab6
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2015-06-03 13:52:25 +0100

    working template

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,7 @@ var main = (function() {
     function getData(callback) {
         var url = 'http://visuals.guim.co.uk/spreadsheetdata/1-xaQYSi2vCehcIhs_p1bNWLed1PQb3nUSmVkRrOciFw.json';
         $.get(url, function(data) {
-            callback(data.sheets.missions);
+            callback(data.sheets);
         });
     }
 
