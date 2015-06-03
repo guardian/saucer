4d8319ea007a61bf17d086128adfc3408abf2069
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2015-06-03 13:42:29 +0100

    Loading data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,26 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+var main = (function() {
+
+    function getData(callback) {
+        var url = 'http://visuals.guim.co.uk/spreadsheetdata/1-xaQYSi2vCehcIhs_p1bNWLed1PQb3nUSmVkRrOciFw.json';
+        $.get(url, function(data) {
+            console.log(data);
+        });
+    }
+
+    function loadTemplates(data) {
+      $.get('templates/mission-teaser.mst', function(template) {
+        var rendered = Mustache.render(template, data);
+        $('#mission-teaser').html(rendered);
+      });
+    }
+
+  function run() {
+    getData(loadTemplates);
+  }
+
+  return {
+    runApp: run
+  }
+}());
+
