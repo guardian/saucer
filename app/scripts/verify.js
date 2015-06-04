3437c16794b8d81a08a9a132398e3d67b610c980
Will Franklin <will.franklin@guardian.co.uk>
2015-06-04 15:23:52 +0100

    Baked data

--- app/scripts/verify.js
+++ app/scripts/verify.js
@@ -8,8 +8,9 @@
     return m;
   }
 
-  load('video-1').then(function(data) {
+  //load('video-1').then(function(data) {
 
+    var data = [{"time":"257.795008","tag":"Cover face"},{"time":"229.328514","tag":"SUV"},{"time":"217.068882","tag":"Car"},{"time":"214.654103","tag":"Explosion"},{"time":"211.867823","tag":"Firework"},{"time":"189.275736","tag":"Smoke"},{"time":"183.749614","tag":"Firework"},{"time":"176.969666","tag":"Smoke"},{"time":"168.239322","tag":"Firework"},{"time":"167.751723","tag":"Firework"},{"time":"146.668871","tag":"Surrender"},{"time":"142.141166","tag":"Raised arms"},{"time":"88.18021","tag":"Citizens"},{"time":"87.158574","tag":"Raised arms"},{"time":"66.075722","tag":"Gas mask"},{"time":"63.800257","tag":"Cupcake"},{"time":"58.436668","tag":"Vehicle"},{"time":"31.316876","tag":"Surrender"},{"time":"23.79392","tag":"Raised arms"},{"time":"8.213971","tag":"Citizens"}]
     /*var data = [{"time": 273.203315, "tag": "Firing into houses"},
       {"time": 208.22751, "tag": "Smoke grenades"},
       {"time": 207.10596, "tag": "Rubber bullets"},
@@ -55,6 +56,6 @@
     }
 
     setInterval(showTags, 500);
-  });
+  //});
 
 })();
